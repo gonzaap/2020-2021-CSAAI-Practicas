6cb7d628dc57a3ba64b11dcf9669d596c44cc620
gonzaap <gonzatrax@gmail.com>
2021-04-08 17:09:21 +0200

    Ejemplo 7

--- P2/S4/Ej-06.js
+++ P2/S4/Ej-06.js
@@ -1,16 +1,10 @@
-//-- Manejador del evento click sobre el párrafo test
-//-- Cada vez aue se hace click en el se invoca a esta funcion
-function manejador_parrafo()
-{
-  console.log("Click sobre el párrafo...")
-}
-
 console.log("Ejecutando js...")
 
 //-- Leer el párrafo identificado como test
 const test = document.getElementById('test')
 
 //-- Configurar el manejador para el evento de
-//-- pulsación de botón: que se ejecute la
-//-- funcion manejador_parrafo()
-test.onclick = manejador_parrafo;
+//-- pulsación de botón
+test.onclick = function () {
+  console.log("Click sobre el párrafo...")
+}
