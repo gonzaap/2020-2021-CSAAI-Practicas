0c3d61366a000591289a0549e194c7f421a406da
gonzaap <gonzatrax@gmail.com>
2021-04-29 12:52:59 +0200

    Pelota implementada

--- P3/index.js
+++ P3/index.js
@@ -1,32 +1,50 @@
 console.log("Ejecutando JS...");
 
 const canvas = document.getElementById("canvas");
-var ctx = canvas.getContext("2d");
+const ctx = canvas.getContext("2d");
 
 //-- Definir el tamaño del canvas
 canvas.width = 900;
 canvas.height = 800;
+
+let speed = 5;
+//Definiendo pelota
+    var pelotax= canvas.width/2;
+    var pelotay= canvas.height -300;
+    var dx= speed;
+    var dy= -speed +1;
+    var radius= 7;
+    
+function drawPelota() {
+    ctx.beginPath();
+    ctx.arc(pelotax, pelotay, radius, 0, Math.PI*2,true);
+    ctx.fillStyle = "#FFFFFF";
+    ctx.fill();
+    ctx.closePath();
+}
 
 //Definiendo raqueta
 var anchoRaqueta = 100;
 var altoRaqueta = 20; 
 var x = (canvas.width-anchoRaqueta)/2;
 
+
 function drawRaqueta() {
     ctx.beginPath();
     ctx.rect(canvas.width/2 -50,canvas.height -50 , anchoRaqueta, altoRaqueta);
     ctx.fillStyle = "#FFFFFF";
-    ctx.fill();  
+    ctx.fill();
+    ctx.closePath();  
 }
-
-    ctx.closePath();
-   
-
+  
  function draw(){
      ctx.clearRect(0,0,canvas.width,canvas.height);
      drawRaqueta();
-     
-     
+     drawPelota();
+     pelotax += dx;
+     pelotay += dy;
+
+     requestAnimationFrame(draw);
  }
  
 
