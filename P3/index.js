0825dbcf5098f708fd0d54af1db84519b260a853
gonzaap <gonzatrax@gmail.com>
2021-04-29 11:50:21 +0200

    Actualizando repositorio, probando nuevos codigos

--- P3/index.js
+++ P3/index.js
@@ -1,13 +1,33 @@
-var raqueta;
+console.log("Ejecutando JS...");
+
 const canvas = document.getElementById("canvas");
-//Creando canvas
-function setup(){
-    canvas.width = 900;
-    canvas.height = 600;
-    raqueta = new raqueta();
+var ctx = canvas.getContext("2d");
+
+//-- Definir el tamaño del canvas
+canvas.width = 900;
+canvas.height = 800;
+
+//Definiendo raqueta
+var anchoRaqueta = 100;
+var altoRaqueta = 20; 
+var x = (canvas.width-anchoRaqueta)/2;
+
+function drawRaqueta() {
+    ctx.beginPath();
+    ctx.rect(canvas.width/2 -50,canvas.height -50 , anchoRaqueta, altoRaqueta);
+    ctx.fillStyle = "#FFFFFF";
+    ctx.fill();  
 }
-//Dibujando elementos
-function draw(){
-    player.move();
-    player.show();
-}
+
+    ctx.closePath();
+   
+
+ function draw(){
+     ctx.clearRect(0,0,canvas.width,canvas.height);
+     drawRaqueta();
+     
+     
+ }
+ 
+
+ draw();
